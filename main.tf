20d77853ec4564e33bf02f751096663653ed6260
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-09-29 21:50:33 +0200

    Add rds module

--- main.tf
+++ main.tf
@@ -22,3 +22,38 @@ module "vpc" {
     "Name" = "${var.app_name}-db-subnet"
   }
 }
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 3.0"
+
+  identifier = "demodb"
+
+  engine            = "postgres"
+  engine_version    = "13.4"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 5
+
+  name     = "${var.app_name}db"
+  username = "demoapp"
+  password = "ThisIsSomeSickProtection"
+  port     = "5432"
+
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  tags = {
+    Name = "${var.app_name}-rds"
+  }
+
+  subnet_ids = module.vpc.database_subnets
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+
+  deletion_protection = true
+
+  create_db_option_group = false
+  create_db_parameter_group = false
+}
