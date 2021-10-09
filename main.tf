86caadf12fd1560d8530196c2a5f91dfa47547c2
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-10-09 15:56:50 +0200

    Add remote state

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-bucket-jr"
+        key = "terraform-state-bucket-jr/state.tfstate"
+        region = "eu-central-1"
+    }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
