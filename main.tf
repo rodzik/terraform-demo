515f6e688c22ef4be76c460196d9c95c230943f0
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-10-10 19:58:38 +0200

    Add vpc with pub subnet

--- main.tf
+++ main.tf
@@ -9,3 +9,27 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags = {
+      Name = "${var.app_name}"
+    }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = aws_vpc.vpc.id
+    tags = {
+      Name = "${var.app_name}"
+    }
+}
+
+resource "aws_subnet" "pub_subnet" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.10.0/24"
+    tags = {
+      Name = "${var.app_name}"
+    }
+}
