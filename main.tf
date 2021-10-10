477c1095f196854e22c3861c167fb5ca4d1b8689
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-10-10 21:45:57 +0200

    Add autoscaling group

--- main.tf
+++ main.tf
@@ -121,3 +121,23 @@ resource "aws_iam_instance_profile" "ecs_agent" {
   name = "ecs-agent"
   role = aws_iam_role.ecs_agent.name
 }
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+    image_id             = "ami-094d4d00fd7462815"
+    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+    security_groups      = [aws_security_group.ecs_sg.id]
+    user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
+    instance_type        = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+    name                      = "asg"
+    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
+    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+
+    desired_capacity          = 1
+    min_size                  = 1
+    max_size                  = 1
+    health_check_grace_period = 300
+    health_check_type         = "EC2"
+}
