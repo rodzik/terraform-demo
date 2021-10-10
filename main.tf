a9f510f57140c92abaf8f37f9a0feec5671df2a8
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-10-10 21:19:38 +0200

    Add route table

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-state-bucket-jr"
-        key = "terraform-state-bucket-jr/state.tfstate"
-        region = "eu-central-1"
-    }
+  backend "s3" {
+    bucket = "terraform-state-bucket-jr"
+    key = "terraform-state-bucket-jr/state.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
@@ -11,25 +11,43 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
-    tags = {
-      Name = "${var.app_name}"
-    }
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.app_name}"
+  }
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.vpc.id
-    tags = {
-      Name = "${var.app_name}"
-    }
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.app_name}"
+  }
 }
 
 resource "aws_subnet" "pub_subnet" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.0.10.0/24"
-    tags = {
-      Name = "${var.app_name}"
-    }
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.10.0/24"
+  tags = {
+    Name = "${var.app_name}"
+  }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.vpc.id
+
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "${var.app_name}"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.public.id
 }
