854dbe4a3f43bf566bcc510819618fae61baf1df
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-10-10 21:22:28 +0200

    Add security groups

--- main.tf
+++ main.tf
@@ -51,3 +51,47 @@ resource "aws_route_table_association" "route_table_association" {
   subnet_id      = aws_subnet.pub_subnet.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "ecs_sg" {
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "rdc_sg" {
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        protocol        = "tcp"
+        from_port       = 5432
+        to_port         = 5432
+        cidr_blocks     = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.ecs_sg.id]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
